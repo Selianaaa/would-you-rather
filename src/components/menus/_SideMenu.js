a6b69a7d8cdf0cec63fab3589d9152c31c84d650
seliana <cany96@bk.ru>
2021-08-15 15:23:26 +0300

    feat: convert jsx to js

--- src/components/menus/_SideMenu.js
+++ src/components/menus/_SideMenu.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { NavLink, useHistory } from 'react-router-dom';
